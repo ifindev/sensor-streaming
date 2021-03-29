d393ef8af8c813da1e5892e3443e0f7d0959bdba
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-03-29 23:06:41 +0700

    Fixed axios post structure and add http to endpoint

--- client/client.js
+++ client/client.js
@@ -2,44 +2,38 @@ const axios = require('axios');
 const sensorData = require('./src/utils/generateSensorData');
 
 /* backend end-point */
-let testEndpoint = 'https://jsonplaceholder.typicode.com/posts';
-let endpoint = 'localhost:3000/add';
+const testEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+const endpoint = 'http://localhost:3000/sensor/add';
 
 /* Sensor data from client (e.g. Raspberry pi) */
 let id = 1;
-let temperature = 21.279782079384667;
-let humidity = 87.2525512400796;
-let area = ['roomArea1', 'roomArea2', 'roomArea3'];
-let roomArea = area[0];
 
 setInterval(() => {
-  let data = {
+  const temperature = sensorData.getTemperature();
+  const humidity = sensorData.getHumidity();
+  const area = ['roomArea1', 'roomArea2', 'roomArea3'];
+  const roomArea = area[id % 3];
+
+  const data = {
     id: id,
     temperature: temperature,
     humidity: humidity,
     roomArea: roomArea,
   };
 
-  // request options
-  const options = {
+  axios({
     method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  axios
-    .post(endpoint, options)
+    headers: { 'Content-Type': 'application/json' },
+    url: endpoint,
+    data: data,
+  })
     .then((response) => {
       console.log(response.data);
-      // Set new value to data
+      console.log('response');
+
       id += 1;
-      temperature = sensorData.getTemperature();
-      humidity = sensorData.getHumidity();
-      roomArea = area[id % 3];
     })
-    .catch(function (error) {
-      console.log(error.data);
+    .catch((error) => {
+      console.log(error.message);
     });
 }, 2000);
