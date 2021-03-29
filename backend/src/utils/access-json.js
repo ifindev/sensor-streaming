3a815b5714612d8a4de11dfc88b4654b799e783a
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-03-29 19:15:32 +0700

    Add test data and modify readFromJSON

--- backend/src/utils/access-json.js
+++ backend/src/utils/access-json.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 module.exports = {
   readFromJSON: (filename) => {
     const jsonData = fs.readFileSync(filename);
-    return JSON.parse(jsonData);
+    try {
+      return JSON.parse(jsonData);
+    } catch (e) {
+      console.log('Make sure json file contain at least an empty array!!');
+      return [];
+    }
   },
 
   storeToJSON: (filename, data) => {
