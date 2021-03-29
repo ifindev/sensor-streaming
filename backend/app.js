f48dd9266320338d488d4f0dc58b7a038dd26df4
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-03-29 23:02:42 +0700

    Fix http port number

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const sensorRouter = require('./src/routes/sensor-route');
 app.use('/sensor', sensorRouter);
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Server runs on port ${port}`);
 });
